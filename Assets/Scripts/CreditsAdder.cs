872dcb8c5f621e5fdbc14121c0590761efe58629
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-10-24 11:52:12 +0200

    Initial project. Added basic shop menu scheme.

---
+++ Assets/Scripts/CreditsAdder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsAdder : MonoBehaviour
+{
+
+    private Text creditsAmountLabel;
+    private int creditsAmount;
+
+    void Start()
+    {
+        creditsAmount = 0;
+        creditsAmountLabel = GameObject.Find("YourBalanceText").GetComponent<Text>();
+    }
+
+
+    void Update()
+    {
+        creditsAmountLabel.text = creditsAmount.ToString();
+    }
+
+    public void add5Credits()
+    {
+        creditsAmount += 5;
+    }
+
+    public void add50Credits()
+    {
+        creditsAmount += 50;
+    }
+
+    public void add500Credits()
+    {
+        creditsAmount += 500;
+    }
+}
